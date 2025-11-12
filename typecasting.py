68d39f17116a1152511b7c9c382777287eba7931
RehanLalu25 <laluvmail@gmail.com>
2025-11-12 18:37:48 +0530

    typecasting.py

---
+++ typecasting.py
@@ -0,0 +1,19 @@
+Name = "Rehan"
+Age = 11
+weight = 31.9
+is_student = True
+print("Name:", Name)
+print("Datatype of name is", type(Name))
+print("Age:", Age)
+print("Datatype of age is", type(Age))
+print("weight:", weight)
+print("Datatype of weight is", type(weight))
+print("is_student:", is_student)
+print("Datatype of is_student is", type(is_student))
+print("\n after typecasting")
+Age = str(Age)
+weight = int(weight)
+print("Age:", Age)
+print("Datatype of age is", type(Age))
+print("weight:", weight)
+print("Datatype of weight is", type(weight))
