7afa94d83070bb5c641d677c523ff586ddd9b285
RehanLalu25 <laluvmail@gmail.com>
2025-11-13 20:39:33 +0530

    Percentage.py

---
+++ Percentage.py
@@ -0,0 +1,7 @@
+Maths = int(input("maths:"))
+English = int(input("english:"))
+science = int(input("science:"))
+Hindi = int(input("hindi:"))
+sum = Maths+English+science+Hindi
+Percentage = (sum/400)*100
+print("Percentage=",Percentage)
