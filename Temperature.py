95ceb5cb24e35cf1d0c24221340555b5092925ea
RehanLalu25 <laluvmail@gmail.com>
2025-11-22 11:31:13 +0530

    temperature.py

---
+++ Temperature.py
@@ -0,0 +1,8 @@
+temperature = float(input("Enter the current temperature in °C: "))
+
+if temperature >= 22:
+    print("It's warm enough! Rohan can wear light and soft clothes 😄")
+elif 15 <= temperature < 22:
+    print("It's a bit cool. Rohan can wear light clothes with a light jacket 😊")
+else:
+    print("It's too cold! Rohan should wear warm clothes 🧥")
