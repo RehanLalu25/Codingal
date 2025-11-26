1239306fe3976abcd0916b952c8ca894a6e377c2
RehanLalu25 <laluvmail@gmail.com>
2025-11-26 18:43:31 +0530

    Commit

---
+++ and or.py
@@ -0,0 +1,18 @@
+a = 10
+b = -10
+c = 0
+if a and b and c:
+    print("all the numbers have boolean values as true")
+else:
+    print("all the numbers have boolean values as false")
+
+if a>b or a>0 or b>0:
+    print("either of the number is greater than 0")
+else:
+    print("no number is greater than 0") 
+
+if b > 0 or c > 0:
+    print("either of the number of the is greater than 0 ")
+else:
+    print("no number is greater than 0")
+    
---
+++ not.py
@@ -0,0 +1,14 @@
+a = 67
+b = 69
+c = 69
+print(a!=b)
+print(b!=c)
+if a!=b:
+    print("a and b are different")
+if(a==5)!= (b==69):
+    print("hello!") 
+x = int(input("Enter the Number"))
+if x%2!= 0:
+    print("x is not an even number")
+else:
+    print("x is an even number")    
