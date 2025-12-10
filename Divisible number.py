65c0a9b03bf60f7089fb4e40ae7d889329e676cc
RehanLalu25 <laluvmail@gmail.com>
2025-12-10 18:31:50 +0530

    Divisble number .py

---
+++ Divisible number.py
@@ -0,0 +1,8 @@
+print("Enter the numenator")
+numn = int(input())
+print("Enter the denominator")
+numd = int(input())
+if numn%numd== 0:
+    print("numenator is divisble by the the denominator.")
+else:
+    print("numenator is not divisble by the denominator.")    
