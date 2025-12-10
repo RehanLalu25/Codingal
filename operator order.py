eda1773ca9e7a20c153df56cfcd0e19b84c69792
RehanLalu25 <laluvmail@gmail.com>
2025-12-10 18:23:54 +0530

    operator order.py

---
+++ operator order.py
@@ -0,0 +1,13 @@
+v = 4
+w = 5
+x = 8
+y = 2
+z = 0
+z = (v+w)*x/y
+print("the new value of z is ",z)
+name = "rehan"
+age = 11
+if name== "rehan" or name== "preethi" and age>=10:
+    print("Hello!")
+else:
+    print("Bye!")    
