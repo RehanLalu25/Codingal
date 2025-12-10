7456f73a0a3040e9752e3e58ddbaf0b3bdb90f3d
RehanLalu25 <laluvmail@gmail.com>
2025-12-10 18:39:52 +0530

    mean.py

---
+++ mean.py
@@ -0,0 +1,10 @@
+mean1 = 38
+wrongnumber = 36
+correctnumber = 56
+Totalnumber = 40
+sum = mean1*Totalnumber
+print("the sum of 40 number is sum",sum)
+num2 = sum-(wrongnumber- correctnumber)
+print("num2")
+mean2 = num2/Totalnumber
+print("the correct mean is ",mean2)
