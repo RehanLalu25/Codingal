ed77f2ff6a7fdc3bfe4aa99a9e6d6052536376f5
RehanLalu25 <laluvmail@gmail.com>
2025-12-11 14:59:06 +0530

    Number Swap.py

---
+++ Number swap.py
@@ -0,0 +1,15 @@
+a = int(input("Enter first value: "))
+b = int(input("Enter second value: "))
+c = int(input("Enter third value: "))
+
+print("\nBefore swapping:")
+print("a =", a)
+print("b =", b)
+print("c =", c)
+
+a, b, c = b, c, a
+
+print("\nAfter swapping:")
+print("a =", a)
+print("b =", b)
+print("c =", c)
