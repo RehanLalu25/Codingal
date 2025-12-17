407e0b13f1c714251259f54057ba86e012ddd915
RehanLalu25 <laluvmail@gmail.com>
2025-12-17 18:31:15 +0530

    reverse a string.py

---
+++ reverse a string.py
@@ -0,0 +1,6 @@
+String = input("Please enter your own string!!!!!")
+string2 = ('')
+for i in String:
+    string2 = i + string2
+print("original string is ",String) 
+print("reverse string is ",string2)   
