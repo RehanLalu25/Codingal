dde2337251478204c1cfef60c367e1926546222f
RehanLalu25 <laluvmail@gmail.com>
2026-01-07 18:43:04 +0530

    Spiral pattern.py

---
+++ spiral pattern.py
@@ -0,0 +1,13 @@
+import turtle 
+Pattern = turtle.Screen()
+Pattern.bgcolor("light blue")
+Pattern.title("turtle")
+mypen = turtle.Turtle()
+size = 0
+while True:
+    for i in range(4):
+        mypen.fd(size + 1)
+        mypen.left(90)
+        size = size - 5 
+    size = size + 1
+        
