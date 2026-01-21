595979bf5adca89904a1ed225dd8615da85f5918
RehanLalu25 <laluvmail@gmail.com>
2026-01-21 19:29:05 +0530

    pass.py

---
+++ pass.py
@@ -0,0 +1,11 @@
+for s in range(10):
+    if s%20==0:
+        print("twist")
+    elif s%15==0:
+        pass
+    elif s%5==0:
+        print("zzzz")
+    elif s%3==0:
+        print("buzzzz")
+    else:
+        print(s)            
