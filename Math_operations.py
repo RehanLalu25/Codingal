65a4c7d0649895848660bf20c119c6845715dd8b
RehanLalu25 <laluvmail@gmail.com>
2026-01-22 20:02:20 +0530

    Math_operations.py

---
+++ Math_operations.py
@@ -0,0 +1,5 @@
+import math
+print("the floor and ceiling values of 67.67 are: "+ str(math.ceil(67.67))+"," + str(math.floor(67.67)))
+x = 10
+y = -15
+print("The value of x after copying the sign from y is: " + str(math.copysign(x,y)) + ',' + str(math.fabs(96)))
