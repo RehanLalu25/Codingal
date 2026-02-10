bd795d7a52699bd26c1a3ba6a895307e7c6bdddd
RehanLalu25 <laluvmail@gmail.com>
2026-02-10 12:41:19 +0530

    Tuple_Product.py

--- Get_the_country_code.py
+++ Get_the_country_code.py
@@ -4,4 +4,4 @@ SPCMcountry_code = {'India' : '0091',
 print("Country code for India =")
 print(SPCMcountry_code.get('India', 'Not Found'))
 print("Country code for Japan =")
-print(SPCMcountry_code.get('Japan', 'Not Found'))
+print(SPCMcountry_code.get('JapanAAAAAAAAAAAAAA', 'Not Found'))
---
+++ Tuple_Product.py
@@ -0,0 +1,13 @@
+tup1 = (4, 3, 2, 2, -1, 18)
+tup2 = (2, 4, 8, 8, 3, 2, 9)
+
+product1 = 1
+for i in tup1:
+    product1 *= i
+
+product2 = 1
+for i in tup2:
+    product2 *= i
+
+print("Product of tup1:", product1)
+print("Product of tup2:", product2)
