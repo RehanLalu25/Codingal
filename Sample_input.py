22122fbe5ea11937f03dda4937db2f0d883bc26f
RehanLalu25 <laluvmail@gmail.com>
2026-02-14 00:20:00 +0530

    Sample_imput.py

---
+++ Sample_input.py
@@ -0,0 +1,5 @@
+Rehan =  int(input("Enter the first number of choice! "))
+Rehan2 = int(input("Enter the second number of choice! "))
+SumOfTheNumbers = Rehan + Rehan2
+print("The sum of the numbers you just entered = ",SumOfTheNumbers)
+
