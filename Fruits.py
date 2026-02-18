d41412534fcb33953919ed0e06fad1fbdf22d19c
RehanLalu25 <laluvmail@gmail.com>
2026-02-18 20:32:01 +0530

    Fruits.py

---
+++ Fruits.py
@@ -0,0 +1,8 @@
+# Original list
+fruits = ["apple", "banana", "mango", "grapes", "orange"]
+
+# Capitalize first letter using list comprehension
+updated_fruits = [fruit.capitalize() for fruit in fruits]
+
+print("Original list:", fruits)
+print("Updated list:", updated_fruits)
