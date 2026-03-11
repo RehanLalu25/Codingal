47095affd1d33c4a7eeecabeb720b78a61c16c29
RehanLalu25 <laluvmail@gmail.com>
2026-03-11 18:40:45 +0530

    Computer_price.py

---
+++ Computer_price.py
@@ -0,0 +1,13 @@
+class Computer :
+    def __init__(self):
+        self.__maxprice =900
+    def sell(self):
+        print("Selling Price :{}".format(self.__maxprice))
+    def setMaxPrice(self,price):
+        self.__maxprice = price
+C = Computer()
+C.sell()
+C.__maxprice = 1000
+C.sell()
+C.setMaxPrice(1000)
+C.sell()
